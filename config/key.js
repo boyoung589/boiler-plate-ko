85d4bbec00df1e9523da43afc11b9ae9865b0f05
xmdnlxjqk <boyoung.y589@gmail.com>
2021-12-23 02:09:26 +0900

    changed: 비밀번호 암호화

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
-//환경변수가 배포단계이면 프로드 파일에서 몽고디비 유알엘을 가져오고 아니면 데브에서 가져온다
+//환경변수가 배포단계이면 프로드 파일에서 몽고디비 유알아이을 가져오고 아니면 데브에서 가져온다
 if(process.env.NODE_ENV === 'production'){
     module.exports = require('./prod');
 } else {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const userSchema = mongoose.Schema({
 
@@ -38,6 +41,48 @@ const userSchema = mongoose.Schema({
         //토큰 유효기간
         type: Number
     }
+})
+
+userSchema.pre('save', function(next){
+    //비밀번호를 암호화시킨다.
+    //bcrypt를 가져온다(암호화를 위한 라이브러리)
+    //위에
+    //salt를 이용해서 비밀번호를 암호화해야함
+    //=> salt생성해야함
+    //saltRounds는 salt가 몇글자인지 정해주는 것 
+    const user = this; //위 userSchema의 내용을 가져온다
+    if(user.isModified('password')){
+        //패스워드가 변환될 때만 아래 암호화 함수가 실행되게 함
+        //이 부분을 설정해주지 않으면 이름, 이메일등 다른 데이터를 변경해서 
+        //새로 저장할 때마다 비밀번호 재암호화가 진행됨
+        bcrypt.genSalt(saltRounds, function(err, salt) { //genSalt: salt를 만든다
+            
+            if(err){
+                return next(err); 
+                //next를 하면 "userSchema.pre('save', callback)"의 'save'활동으로 넘어감
+                //index.js의 user.save()부분
+            }
+            const myPlaintextPassword = user.password;
+            bcrypt.hash(myPlaintextPassword, salt, function(err, hash) { 
+                //bcrypt.hash: hashing(암호화)하는 메소드
+                //myPlaintextPassword: 암호화 전 비밀번호
+                //파라미터의 hash: 암호화된 비밀번호
+    
+                if(err){
+                    return next(err);
+                } else{
+                    //hash 성공
+                    user.password = hash;
+                    next();
+                    //이 부분을 위해 userSchema.pre()안에 user = this;를 넣어준 것.
+                }
+            });
+        }); //bcrypt 공식문서의 사용법 참조:https://www.npmjs.com/package/bcrypt
+    } else {
+        next();
+        //비밀번호를 바꾸는 게 아니라 다른 데이터를 post하는 요청이면 pre함수를 빠져나와서 'save'진행
+    }
+    
 })
 
 const User = mongoose.model('User', userSchema) 
