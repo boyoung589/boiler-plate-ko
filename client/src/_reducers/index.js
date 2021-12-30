f67c161274d4656dbb1698908aa173db866312c5
xmdnlxjqk <boyoung.y589@gmail.com>
2021-12-30 23:32:33 +0900

    added: concurrently & redux

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux"; //여러가지 리듀서를 root reducer로 모아주는 것
+//import user from './user_reducer';
+
+const rootReducer = combineReducers({
+    //user
+})
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import Thunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+
+
+const createStoreWithMiddleware = applyMiddleware( promiseMiddleware, Thunk)(createStore)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWithMiddleware(Reducer, 
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__() //리덕스 연결
+        )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
